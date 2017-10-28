903b04c70178995d1bf1adfe0e7c637487974986
AKS1996 <sushsilsharma.ss451@gmail.com>
2017-10-28 20:04:36 +0530

    added view flipper for welcome screen

--- app/src/main/java/com/example/tiapp/MainActivity.java
+++ app/src/main/java/com/example/tiapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.tiapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     // TODO: change status bar color
+
+    private boolean firstView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences settings = getSharedPreferences("prefs", 0);
+        boolean firstRun = settings.getBoolean("firstRun", true);
+        if ( firstRun ) {
+            startActivityForResult(
+                    new Intent(MainActivity.this, WelcomeViewFlipper.class),R.integer.WELCOME_CODE);
+
+        }
+
     }
 }
