b737d5604389e237acd1f152da434631f74b6b7d
AKS1996 <sushsilsharma.ss451@gmail.com>
2017-10-28 19:28:29 +0530

    updated gradle and shit

--- app/src/main/java/com/example/tiapp/MainActivity.java
+++ app/src/main/java/com/example/tiapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // TODO: change status bar color
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
