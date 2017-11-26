758b4436c9cf2c12edc14acea1f2666b69e165dc
akshay <sushsilsharma.ss451@gmail.com>
2017-11-26 13:25:36 +0530

    many small changes

--- app/src/main/java/com/topcoder/timobile/PreStory.java
+++ app/src/main/java/com/topcoder/timobile/PreStory.java
@@ -26,26 +26,25 @@ import java.util.List;
 public class PreStory extends AbstractStep {
 
     private int i = 1;
-    private String[] titles = new String[3];
     ArrayAdapter<String> adapter;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        titles[0] = "1. SET UP YOUR LOCATION";
-        titles[1] = "2. SELECT THE STATES";
-        titles[2] = "3. SELECT THE RACETRACKS";
         Utils.darkenStatusBar(getActivity(),R.color.violet);
 
         i = getArguments().getInt("position", 0);
         View v;
         if (i == 1) {
             v = inflater.inflate(R.layout.pre_story1, container, false);
-            ((TextView) v.findViewById(R.id.toolbarPreStory1)).setText(titles[i-1]);
+            ((TextView) v.findViewById(R.id.toolbarPreStory1)).setText(R.string.title1);
 
         } else {
             v = inflater.inflate(R.layout.pre_story2, container, false);
-            ((TextView) v.findViewById(R.id.toolbarPreStory2)).setText(titles[i-1]);
+            if (i==2)
+                ((TextView) v.findViewById(R.id.toolbarPreStory2)).setText(R.string.title2);
+            else
+                ((TextView) v.findViewById(R.id.toolbarPreStory2)).setText(R.string.title3);
 
             ListView listView = (ListView) v.findViewById(R.id.listViewPreStory);
             List<String> listContents = loadContents(getContext(),i);
